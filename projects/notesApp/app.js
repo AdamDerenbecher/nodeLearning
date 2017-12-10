b70c65f1028818e33ccb23185e75a1b6be58aa21
derenbea <adam.derenbecher@wwt.com>
2017-12-10 14:14:00 -0600

    finished section 3.11

--- projects/notesApp/app.js
+++ projects/notesApp/app.js
@@ -28,7 +28,10 @@ if (command === 'add') {
 } else if (command === 'read') {
   notes.getNote(argv.title);
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  //turnary opperator
+  var message = noteRemoved ? 'Note was removed' : 'Note not found'
+  console.log(message);
 } else {
   console.log('Command not recognized');
 }
